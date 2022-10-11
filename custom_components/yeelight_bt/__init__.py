ccb519455af64b07deb0dbc67ca563af97b2dac9
Oskar Joelsson <oskar.joelsson@gmail.com>
2022-10-11 10:37:50 +0200

    updated espproxy

--- custom_components/yeelight_bt/__init__.py
+++ custom_components/yeelight_bt/__init__.py
@@ -5,15 +5,13 @@ from homeassistant.components.bluetooth import (
     async_ble_device_from_address,
     async_scanner_count,
 )
-from homeassistant.components.bluetooth.models import BluetoothScanningMode
-from homeassistant.components.bluetooth.scanner import create_bleak_scanner
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_MAC
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import DOMAIN
-from mylight import find_device_by_address
+# from mylight import find_device_by_address
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,16 +25,18 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     address = entry.data.get(CONF_MAC)
 
     # try to get ble_device using HA scanner first
-    ble_device = async_ble_device_from_address(hass, address.upper(), connectable=True)
+    ble_device = async_ble_device_from_address(
+        hass, address.upper(), connectable=True)
     _LOGGER.debug(f"BLE device through HA bt: {ble_device}")
-    count = async_scanner_count(hass, connectable=True)
-    _LOGGER.debug(f"Count of BLE devices through HA bt: {count}")
     if ble_device is None:
-        # if bluetooth not enabled, we get ble_device from a bleak scanner returned by HA
-        scanner = create_bleak_scanner(BluetoothScanningMode.ACTIVE, None)
-        ble_device = await scanner.find_device_by_address(address.upper(), timeout=15)
-        _LOGGER.debug(f"BLE device using bleak scanner through HA: {ble_device}")
-    if not ble_device:
+        count_scanners = async_scanner_count(hass, connectable=True)
+        _LOGGER.debug(f"Count of BLE scanners in HA bt: {count_scanners}")
+        if count_scanners < 1:
+            raise ConfigEntryNotReady(
+                "No bluetooth scanner detected. \
+                Enable the bluetooth integration or ensure an esphome device \
+                is running as a bluetooth proxy"
+            )
         raise ConfigEntryNotReady(
             f"Could not find MyLight with address {address}")
 
